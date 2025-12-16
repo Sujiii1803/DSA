32b5b6dc40b33cb31cf71de5c04887041f275593
Sujiii1803 <sujiii1803@gmail.com>
2025-12-16 15:37:56 +0530

    Day 1: Solved Two Sum, Contains Duplicate, Best Time to Buy and Sell Stock (Arrays)

---
+++ src/LeetCode/Arrays/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+package LeetCode.Arrays;
+
+public class BestTimeToBuyAndSellStock
+{
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            int maxProfit = 0;
+            int minPrice = Integer.MAX_VALUE;
+            for(int price : prices)
+            {
+                if(price < minPrice){
+                    minPrice = price;
+                }
+                else{
+                    maxProfit = Math.max(maxProfit,price-minPrice);
+                }
+            }
+            return maxProfit;
+        }
+}
