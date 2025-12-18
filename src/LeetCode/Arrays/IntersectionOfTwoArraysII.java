0835f8673d095dff5e4fbb4d2c2c129164e878bc
Sujiii1803 <sujiii1803@gmail.com>
2025-12-18 06:54:44 +0530

    Day 3: Solved IntersectionOfTwoArraysII, PlusOne, TwoSumII

---
+++ src/LeetCode/Arrays/IntersectionOfTwoArraysII.java
@@ -0,0 +1,34 @@
+package LeetCode.Arrays;
+import java.util.*;
+public class IntersectionOfTwoArraysII
+{
+        public int[] intersect(int[] nums1, int[] nums2)
+        {
+            // Always build map on smaller array
+            if (nums1.length > nums2.length)
+                return intersect(nums2, nums1);
+
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int num : nums1) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+
+            List<Integer> result = new ArrayList<>();
+
+            for (int num : nums2) {
+                if (map.containsKey(num) && map.get(num) > 0) {
+                    result.add(num);
+                    map.put(num, map.get(num) - 1);
+                }
+            }
+
+            // Convert list to array
+            int[] ans = new int[result.size()];
+            for (int i = 0; i < result.size(); i++) {
+                ans[i] = result.get(i);
+            }
+
+            return ans;
+        }
+}
